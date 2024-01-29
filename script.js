6591127abf57ff0e700257da2d048423e209374e
Shreyakaushik2005 <122819914+Shreyakaushik2005@users.noreply.github.com>
2024-01-29 11:41:45 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var discoverButton = document.querySelector('.content button');
-    var additionalContent = document.querySelector('.additional-content');
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
 
-    discoverButton.addEventListener('click', function () {
-        // Toggle the visibility of the additional content section
-        additionalContent.classList.toggle('visible');
-    });
-});
+function addTask() {
+    if (inputBox.value === '') {
+        alert("You must write something!");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+listContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
